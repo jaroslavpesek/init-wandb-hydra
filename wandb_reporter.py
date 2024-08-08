41f61c7311953e277dbbaf3603236d19ab5b52ce
Jaroslav Pesek <pesekja8@fit.cvut.cz>
2024-08-08 20:31:30 +0900

    remove useless code

--- wandb_reporter.py
+++ wandb_reporter.py
@@ -9,19 +9,14 @@ def wandb_report(true_labels, pred_labels, target_names):
     # Get the unique classes present in true_labels and pred_labels
     present_classes = np.unique(np.concatenate([true_labels, pred_labels]))
 
-    # Filter the target_names and true_labels, pred_labels accordingly
-    filtered_target_names = [target_names[i] for i in present_classes]
-    filtered_true_labels = [present_classes.tolist().index(label) for label in true_labels]
-    filtered_pred_labels = [present_classes.tolist().index(label) for label in pred_labels]
-
-    class_report = classification_report(filtered_true_labels, filtered_pred_labels, target_names=filtered_target_names, zero_division=0).splitlines()
+    class_report = classification_report(true_labels, pred_labels, target_names=target_names, zero_division=0).splitlines()
 
     report_table = []
-    for line in class_report[2:(len(filtered_target_names) + 2)]:
+    for line in class_report[2:(len(target_names) + 2)]:
         report_table.append(line.split())
 
     wandb.log({
-        f"eval/cm": wandb.plot.confusion_matrix(y_true=filtered_true_labels, preds=filtered_pred_labels, class_names=filtered_target_names),
+        f"eval/cm": wandb.plot.confusion_matrix(y_true=true_labels, preds=pred_labels, class_names=target_names),
         f"eval/report": wandb.Table(data=report_table, columns=report_columns),
     })
 
